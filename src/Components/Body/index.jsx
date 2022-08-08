5a684fd3ef8e561d06b806e6f33a35158b8e354b
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-08-07 22:44:28 -0300

    feat: cp body
    criado componente body

---
+++ src/Components/Body/index.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../Provider/Products";
+import Cards from "../Cards";
+import "../../Assets/Styles/Css/body.css"
+
+const Body = () => {
+    const { productsAll } = useContext(ProductsContext);
+    
+    return (
+        <main className="container--body">
+            <div className="body-img">
+                <h3>Todas as Frutas</h3>
+            </div>
+            <Cards card={productsAll} />
+        </main>
+    )
+}
+export default Body 
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes as RoutePages } from "react-router-dom"
+import Body from "../Components/Body"
 import Cards from "../Components/Cards"
 import Header from "../Components/Header"
 
 const Routes = () => {
     return (
         <RoutePages>
-            <Route exact path="/" element={<Header />} />
+            <Route exact path="/" element={<Body />} />
         </RoutePages>
     )
 }
