fe72df27454c6e364d51efbfd137b2a0f4042149
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-08-08 17:13:14 -0300

    feat: criado cp Modal Card
    criado componente que irá mostar valores nutricionais

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import "../../Assets/Styles/Css/cards.css";
 import { CartContext } from "../../Provider/Cart";
+import ModalCard from "../ModalCard";
 
 
 const Cards = ({ card }) => {
@@ -8,30 +9,40 @@ const Cards = ({ card }) => {
     const { addCartProducts } = useContext(CartContext)
 
 
+
     return (
         <section className="container--cards">
             {card.map((el) => {
                 const { name, id, genus, family } = el
                 return (
-                    <ul key={id} className="cards--list-unitary">
-                        <li className="list-unitary">
-                            <span>Nome:</span>
-                            <p>{name}</p>
-                        </li>
-                        <li className="list-unitary">
-                            <span>Gênero:</span>
-                            <p>{genus}</p>
-                        </li>
-                        <li className="list-unitary">
-                            <span>Família:</span>
-                            <p>{family}</p>
-                        </li>
-                        <li className="list-btn">
-                            <button onClick={() => addCartProducts(el)}>
-                                <p>Adicionar</p>
-                            </button>
-                        </li>                       
-                    </ul>
+                    <div key={id}  className="cards-list">
+                        <ul className="cards--list-unitary">
+                            <li className="list-unitary">
+                                <span>Nome:</span>
+                                <p>{name}</p>
+                            </li>
+                            <li className="list-unitary">
+                                <span>Gênero:</span>
+                                <p>{genus}</p>
+                            </li>
+                            <li className="list-unitary">
+                                <span>Família:</span>
+                                <p>{family}</p>
+                            </li>
+                            <li className="list-btn">
+                                <button onClick={() => addCartProducts(el)}>
+                                    <p>Adicionar</p>
+                                </button>
+                            </li>
+                        </ul>
+                        <ModalCard
+                            calories={el.nutritions.calories}
+                            carbohydrates={el.nutritions.carbohydrates}
+                            fat={el.nutritions.fat}
+                            protein={el.nutritions.protein}
+                            sugar={el.nutritions.sugar} />
+                    </div>
+
 
                 )
             })}
